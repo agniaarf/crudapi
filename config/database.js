517861ff6a8bac69fa1a793036678b4db27a8333
muhammadfaiz66 <muhammadfaiz7130@gmail.com>
2020-09-28 15:57:46 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: "postgres://zikiahnxjkjcpc:008c0a2e79abbbaf3ccb8043b96ec82a4a566598831e8e66db6f608fe75095c0@ec2-52-72-34-184.compute-1.amazonaws.com:5432/d717g3edne088o",
   ssl: {
     rejectUnauthorized: false
   }
