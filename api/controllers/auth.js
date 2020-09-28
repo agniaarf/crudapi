e249e08e985db95ad32618338498de8a11172ba6
muhammadfaiz66 <muhammadfaiz7130@gmail.com>
2020-09-28 16:24:36 +0700

    update link authentication

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -9,11 +9,11 @@ module.exports.authenticate = function(req,res,next){
           next(err);
           res.status(404).send({status:"not found", message: "Invalid userid", user:null});
        } else {
-           if(userInfo.length > 0){
-            //    console.log("userinfo",userInfo)
-               if(userInfo[0].password === req.body.password){
-                   const token = jwt.sign({id: userInfo[0]._id}, req.app.get('secretKey'), { expiresIn: '1y' });
-                   res.status(200).send({status:"success", message: "user found!!!", user: userInfo[0], token:"Bearer " + token});
+           if(userInfo.rows.length > 0){
+            //    console.log("userinfo.rows",userInfo.rows)
+               if(userInfo.rows[0].password === req.body.password){
+                   const token = jwt.sign({id: userInfo.rows[0]._id}, req.app.get('secretKey'), { expiresIn: '1y' });
+                   res.status(200).send({status:"success", message: "user found!!!", user: userInfo.rows[0], token:"Bearer " + token});
                }
                else{
                    res.status(404).send({status:"not found", message: "Invalid password", user:null});
